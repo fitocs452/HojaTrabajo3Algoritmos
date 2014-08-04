0166606da50292ce6bed6b2c11edb77e2d762a8c
Adolfo <Adolfo@AdolfoPC>
2014-08-03 22:09:10 -0600

    Documentación MergeSort

--- src/ordenamiento/MergeSort.java
+++ src/ordenamiento/MergeSort.java
@@ -22,22 +22,22 @@ public class MergeSort extends Ordenador{
     }
 
     public void mergesort(int low, int high) {
-    // check if low is smaller then high, if not then the array is sorted
+    // En caso de que izq sea menor que der, entonces la cadena tiene un largo de 0 o uno entonces esta ordenada.
         if (low < high) {
-            // Get the index of the element which is in the middle
+            // Se calcula el indice del elemento medio de la cadena para poder ordenar
             int middle = low + (high - low) / 2;
-            // Sort the left side of the array
+            // Ordenar la parte izquierda
             mergesort(low, middle);
-            // Sort the right side of the array
+            // Ordena la parte derecha de la cadena
             mergesort(middle + 1, high);
-            // Combine them both
+            // Combina las dos partess
             valores = merge(low, middle, high);
         }
     }
 
     public int[] merge(int low, int middle, int high) {
 
-        // Copy both parts into the helper array
+        // copia las dos partes en una sola
         for (int i = low; i <= high; i++) {
             helper[i] = numbers[i];
         }
@@ -45,9 +45,9 @@ public class MergeSort extends Ordenador{
         int i = low;
         int j = middle + 1;
         int k = low;
-        // Copy the smallest values from either the left or the right side back
-        // to the original array
+        // copiamos los valores mas pequeños de ambos lados a la cadena original
         while (i <= middle && j <= high) {
+             //comparacion para saber cual es mas pequeño en la segunda mitad
             if (helper[i] <= helper[j]) {
                 numbers[k] = helper[i];
                 i++;
@@ -57,7 +57,7 @@ public class MergeSort extends Ordenador{
             }
             k++;
         }
-        // Copy the rest of the left side of the array into the target array
+        // Copiamos la otra mitad
         while (i <= middle) {
             numbers[k] = helper[i];
             k++;
